864e58a76467ccb9b71060ce748db99e79bf115e
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-04-09 23:15:31 +0200

    nice

--- VoidRift/HelloKotlin/src/Main.kt
+++ VoidRift/HelloKotlin/src/Main.kt
@@ -1,13 +1,25 @@
 
 fun main(args : Array<String>) {
 
+	// kotlin class
 	var h = Hello()
 	h.say()
 	println(h.add(1,2))
+
+	// java class
+	println(Math.add(5, 6))
 
 	var obj = object {
 		var value = 4
 	}
 
 	println(obj.value)
+
+
+	var arr = arrayOf(
+			1, 2, 3
+	)
+
+	println("arr[2] = " + arr[2])
+
 }
---
+++ VoidRift/HelloKotlin/src/Math.java
@@ -0,0 +1,7 @@
+public class Math {
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+}

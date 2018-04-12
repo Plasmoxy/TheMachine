2eb3e43896c4b02c694c94c1d2c547675e2894ab
Plasmoxy <sebohacker@gmail.com>
2018-04-12 17:55:04 +0200

    some node

---
+++ WeirdStuff/socketio_alpha/node/server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var http = require('http');
+var io = require('socket.io');
+
+var exec = require('child_process').exec;
+
+console.log("XD")
--- WeirdStuff/socketio_alpha/src/main/kotlin/Main.kt
+++ WeirdStuff/socketio_alpha/src/main/kotlin/Main.kt
@@ -1,4 +1,25 @@
 
+import io.socket.client.IO
+import io.socket.client.Socket
+
+
+var socket = IO.socket("http://localhost:5999")
+
 fun main(args : Array<String>) {
 	Hello.hi()
+
+	socket.on(Socket.EVENT_CONNECT, {
+		
+		socket.emit("foo", "hi")
+		
+	}).on("event", {
+		
+		
+		
+	}).on(Socket.EVENT_DISCONNECT, {
+		
+		
+		
+	})
+	
 }

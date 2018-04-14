e9ff0b6e8722034002f6c0d825dfe48cd7c777e8
Plasmoxy <sebohacker@gmail.com>
2018-04-14 20:59:09 +0200

    gudd

---
+++ LIBGDX/Junk/HelloWorld/desktop/src/io/github/plasmoxy/libgdx/helloworld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.plasmoxy.libgdx.helloworld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.plasmoxy.libgdx.helloworld.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}

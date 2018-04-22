a2e1351dabad6de85f37f11eec024a93e9962b68
Plasmoxy <sebohacker@gmail.com>
2018-04-22 22:32:30 +0200

    tostringe

---
+++ Java/HelloAgain/src/ToString.java
@@ -0,0 +1,13 @@
+public class ToString {
+
+	public static void main(String[] args) {
+
+		String a = (new Object()).toString();
+		System.out.println(a);
+
+		System.out.println((new Object()).toString());
+
+		System.out.println(new Object());
+		
+	}
+}

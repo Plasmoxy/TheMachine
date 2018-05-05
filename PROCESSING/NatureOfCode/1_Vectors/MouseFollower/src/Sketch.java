4aa1f66ad02e48908db77330a8af9c4c8bbf582d
Plasmoxy <sebohacker@gmail.com>
2018-05-05 16:18:51 +0200

    mf

---
+++ PROCESSING/NatureOfCode/1_Vectors/MouseFollower/src/Sketch.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("Sketch");
+	}
+	
+	
+	
+	
+
+	@Override public void settings() {
+		size(400, 400);
+	}
+
+	@Override public void setup() {
+		
+	}
+
+	@Override public void draw() {
+		
+	}
+}

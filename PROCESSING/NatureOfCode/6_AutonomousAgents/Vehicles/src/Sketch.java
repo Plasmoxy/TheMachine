b26e03e5880d56e776a1e4f69f03af5fb00f4d81
Plasmoxy <sebohacker@gmail.com>
2018-05-08 22:39:47 +0200

    ey

--- PROCESSING/NatureOfCode/6_AutonomousAgents/Vehicles/src/Sketch.java
+++ PROCESSING/NatureOfCode/6_AutonomousAgents/Vehicles/src/Sketch.java
@@ -53,7 +53,6 @@ public class Sketch extends PApplet {
 			steeringForce = new PVector();
 		}
 		
-		/*
 		
 		@Override void update() {
 			
@@ -62,37 +61,59 @@ public class Sketch extends PApplet {
 			// F_steer = v_desired - v_current
 			steeringForce.set(desv);
 			steeringForce.sub(vel);
-			//applyForce(steeringForce);
+			applyForce(steeringForce);
 			
 			super.update();
 		}
-		*/
 		
 		@Override void display() {
 			fill(0, 200);
 			noStroke();
 			pushMatrix();
 			translate(pos.x, pos.y);
-			rotate(atan(vel.y/vel.x));
+			rotate(atan2(desv.y, desv.x));
 			triangle(10, 0, -10, -8, -10, 8);
 			popMatrix();
 		}
 		
 	}
 	
+	PVector mouse;
 	Vehicle v;
+	
+	boolean gravityOn = true;
+	PVector gravity = new PVector(0, 1);
 
 	@Override public void settings() {
 		size(400, 400);
 	}
 
 	@Override public void setup() {
+		mouse = new PVector();
 		v = new Vehicle(50, 50);
 	}
 
 	@Override public void draw() {
+		mouse.x = mouseX; mouse.y = mouseY;
 		background(200);
+		
+		
+		if (gravityOn) v.applyForce(gravity);
+		
+		PVector dm = PVector.sub(mouse, v.pos);
+		dm.normalize();
+		dm.mult(1.5f);
+		v.desv.set(dm);
+		
 		v.update();
 		v.display();
+		
+		text("press mouse to toggle gravity", 20, 20);
+		text("Gravity = " + gravityOn, 20, 50);
+	}
+	
+	
+	@Override public void mousePressed() {
+		gravityOn = !gravityOn;
 	}
 }

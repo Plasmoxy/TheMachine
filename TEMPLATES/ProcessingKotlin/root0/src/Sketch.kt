d93cdd3a072a69c47147289b79d1a84d8dee8f3e
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-05-24 11:18:10 +0200

    xdddd

---
+++ TEMPLATES/ProcessingKotlin/root0/src/Sketch.kt
@@ -0,0 +1,22 @@
+import processing.core.PApplet
+
+class Sketch : PApplet() {
+
+
+	override fun settings() {
+		size(400, 400)
+	}
+
+	override fun setup() {
+		
+	}
+
+	override fun draw() {
+		
+	}
+	
+}
+
+fun main(args: Array<String>) {
+	PApplet.main("Sketch")
+}

4dbf6495fd3899fc6bb89b86e41cd42a15189986
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-06-10 22:32:16 +0200

    this program integrates PI

--- WasteLand/src/integral/Integral.kt
+++ WasteLand/src/integral/Integral.kt
@@ -1,10 +1,10 @@
 package integral
 
+import kotlin.math.PI
 import kotlin.math.pow
-import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
-fun integrate(f : (Double) -> Double, a : Double, b : Double, parts : Int) : Double  {
+fun integrate_simple(f : (Double) -> Double, a : Double, b : Double, parts : Long) : Double  {
 	var size = b - a
 	var dx = size/parts
 	
@@ -21,9 +21,35 @@ fun integrate(f : (Double) -> Double, a : Double, b : Double, parts : Int) : Dou
 	return value
 }
 
-fun integrate_round( f : (Double) -> Double, a : Double, b : Double, parts : Int ) : Double {
-	return (integrate(f, a, b, parts)*1000).roundToInt()/1000.0
+fun integrate_trapezoid(f : (Double) -> Double, start : Double, end : Double, parts : Long) : Double  {
+	
+	var percent = 0L
+	
+	// q = (b-a)/n
+	val q = (end-start)/parts
+	var sum = 0.0
+	
+	sum += q * f(start)/2.0
+	sum += q * f(end)/2.0
+	
+	// for k in 1..(n-1)
+	for (k in 1 until parts) {
+		sum += q * f(start + k*q)
+		
+		val cpercent = k*100/parts
+		if (cpercent > percent) {
+			println("$percent %")
+			percent = cpercent
+		}
+		
+	}
+	
+	
+	// return the sum which is the integral
+	return sum
+	
 }
+
 
 fun main(args: Array<String>) {
 	
@@ -37,7 +63,14 @@ fun main(args: Array<String>) {
 	// arc length : L(f) = integral sqrt(1 + (df/dx)^2) dx
 	var arc = { x : Double -> sqrt(1 + (-x/sqrt(1 - x.pow(2))).pow(2) ) }
 	
-	// integrate the arc length function
-	println( integrate(arc, 0.0, 1.0, 10000) )
+	// better arc ( simplified expresion )
+	var betterarc = { x: Double -> sqrt(1.0/(1.0-x.pow(2.0))) }
+	
+	// integrate the arc length function, DOESNT WORK FOR ARC of circle as for infinity
+	// println( 4*integrate_simple(circle, 0.0, 1.0, 10000) )
+	 
+	
+	var my_PI = 4 * integrate_trapezoid( circle, 0.0, 1.0, 1_000_000_000 )
+	println( "\nmy pi     = $my_PI\nnormal pi = ${PI}" )
 	
 }

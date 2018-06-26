3457bee6beb88c7530cc7dac58daf72ba91c4b95
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-06-26 23:20:28 +0200

    lmao

---
+++ WeirdStuff/KotlinDocs/src/BasicSyntax.kt
@@ -0,0 +1,23 @@
+import kotlin.properties.Delegates.observable
+
+fun main(args: Array<String>) {
+	
+	for ( i in 0..10 step 2) print("$i ")
+	println()
+	
+	var numbers = listOf(1, 3, 2, 6, 5)
+	var evens = numbers.filter { x -> x%2==0 }
+	var odds = numbers.filter { x -> x%2>0 }
+	
+	println("numbers : $numbers")
+	println("evens : $evens")
+	println("odds : $odds")
+	
+	
+	var name : String by observable("") { _, _, x -> println("The name is $x") }
+	name = "Seb"
+	
+	
+	
+	
+}

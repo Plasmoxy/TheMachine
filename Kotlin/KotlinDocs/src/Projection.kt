46eaf6c38676daacb8808cebe80d93c20a589bf7
Plasmoxy <sebohacker@gmail.com>
2018-07-10 11:00:48 +0200

    is cool

---
+++ Kotlin/KotlinDocs/src/Projection.kt
@@ -0,0 +1,25 @@
+
+// holder is both producer and consumer
+class Holder<T>(private var x: T) {
+	fun get(): T = x
+	fun set(a: T) {
+		x = a
+	}
+}
+
+
+fun holderCopy(from: Holder<out Any>, to: Holder<in Any>) {
+	to.set(from.get())
+}
+
+fun main(args: Array<String>) {
+	val first = Holder("XD")
+	val second = Holder(Any())
+	
+	// this wont be allowed because the function can do bad things to first
+	// -> i.e. write Int to String variable, which would then throw ClassCastException
+	// so we just project these types as producer / consumer -only
+	println(second.get())
+	holderCopy(first, second)
+	println(second.get())
+}

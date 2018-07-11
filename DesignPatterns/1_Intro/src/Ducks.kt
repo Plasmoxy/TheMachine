7f7741c17d8a7950b25b96f307760d6577b2e41d
Plasmoxy <sebohacker@gmail.com>
2018-07-11 19:07:47 +0200

    delegation pretty nice

---
+++ DesignPatterns/1_Intro/src/Ducks.kt
@@ -0,0 +1,90 @@
+
+interface FlyBehavior { fun fly() }
+interface QuackBehavior { fun quack() }
+
+class FlyNoWay : FlyBehavior {
+	override fun fly() {} // nope cant fly
+}
+
+class FlyWithWings : FlyBehavior {
+	override fun fly() {
+		println("IM FLYING WITH WINGS !!")
+	}
+}
+
+class NoQuack : QuackBehavior {
+	override fun quack() {} // nope cant quack
+}
+
+class Quack : QuackBehavior {
+	override fun quack() {
+		println("QUACK QUACK !")
+	}
+}
+
+class Squeak : QuackBehavior {
+	override fun quack() {
+		println("SQUEAK SQUAEK !!")
+	}
+}
+
+open class Duck(val name: String) {
+	
+	// fly and quack behaviors can change but dont have to be present, they VARY
+	// -> we encapsulate them outside
+	
+	// then create delegation instances here in ducc
+	var flyBehavior: FlyBehavior = FlyWithWings()
+	var quackBehavior: QuackBehavior = Quack()
+	
+	// doesnt change, keep it
+	fun swim() {
+		println("$name is swimming")
+	}
+	
+	// does change, ALL DUCKS so we run it through inheritance
+	open fun display() {
+		println("Hi my dude im a duck with name $name")
+	}
+	
+	// here we DELEGATE methods from the interfaces to our class
+	fun performQuack() {
+		quackBehavior.quack()
+	}
+	
+	fun performFly() {
+		flyBehavior.fly()
+	}
+}
+
+open class RedDuck(name: String) : Duck(name) {
+	
+	init {
+		quackBehavior = NoQuack() // red duck is autist and cannot quack
+	}
+	
+	override fun display() {
+		println("boi im a REDD DUCC xDDDD name= $name")
+	}
+	
+}
+
+open class RetardedDuck(name: String) : Duck(name) {
+	init {
+		// this duck is totally retarded and cant do anything
+		quackBehavior = NoQuack()
+		flyBehavior = FlyNoWay()
+	}
+	
+	override fun display() {
+		println("XAXAAXAXAXA reatard DUCC xDDDD im $name xDDD")
+	}
+}
+
+
+fun main(args: Array<String>) {
+	val d: Duck = RedDuck("Jack") // polymorphism works
+	d.display()
+	d.performFly()
+	d.performQuack()
+}

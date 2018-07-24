30d321c929f802b5332d5f4c8c931f44fb84f14e
Plasmoxy <sebohacker@gmail.com>
2018-07-24 12:53:54 +0200

    hmmscala

--- Kotlin/juncc/Changgg/src/Hmm.kt
+++ Kotlin/juncc/Changgg/src/Hmm.kt
@@ -1,7 +1,17 @@
 
+data class BoxInt(var x: Int) {
+	override fun toString() = x.toString()
+}
+
+fun chang(t: BoxInt) {
+	t.x += 1
+}
 
 fun main(args: Array<String>) {
 
-
+	val x = BoxInt(3)
+	println(x)
+	chang(x)
+	println(x)
 
 }
---
+++ Scala/testtt/src/XD.scala
@@ -0,0 +1,16 @@
+
+
+
+object XD {
+
+  class A {
+    def +(that: A): Unit = {
+      println("XD")
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    new A + new A
+  }
+
+}

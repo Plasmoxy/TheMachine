a9cb380cff89a3bfa368679ee4148487dacee562
Plasmoxy <sebohacker@gmail.com>
2018-07-24 22:43:22 +0200

    kotlinn coole

---
+++ Kotlin/juncc/MutableIteration/src/MutableIteration.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+	val numbers = mutableListOf(1, 2, 3, 4)
+	
+	val iterator = numbers.listIterator()
+	while (iterator.hasNext()) {
+		val num = iterator.next()
+		print("$num ")
+		if (num%2 == 0) {
+			iterator.add(0)
+			print("! ")
+		}
+	}
+	
+	println(numbers)
+
+}
--- Scala/testtt/src/XD.scala
+++ Scala/testtt/src/XD.scala
@@ -4,13 +4,13 @@
 object XD {
 
   class A {
-    def +(that: A): Unit = {
+    def <<(that: A): Unit =  {
       println("XD")
     }
   }
 
   def main(args: Array[String]): Unit = {
-    new A + new A
+    new A << new A
   }
 
 }

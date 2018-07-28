4f410d95832d2a2fa715458863b67383ae8c943e
Plasmoxy <sebohacker@gmail.com>
2018-07-28 17:51:00 +0200

    heyyy

--- Clojure/HelloClojure/src/XD.clj
+++ Clojure/HelloClojure/src/XD.clj
@@ -1,3 +1,23 @@
 (ns XD)
-(println "XD")
-(println (+ 1 (- 3 2)))
+(import java.util.Random)
+
+(defn add [x, y]
+	(+ x y))
+(println
+	(add 1 2))
+
+
+(def random (Random.))
+(defn getRandom [] (.nextInt random 10))
+(println (getRandom))
+
+(def a (fn [] (println "XD")))
+(a)
+
+(dotimes [i 5]
+	(print i))
+(println)
+
+(println
+	(take 5
+				(repeat 2)))

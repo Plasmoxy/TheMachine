ecb0b368d844b4185b5e869fc7d52df6c498dd26
Plasmoxy <sebohacker@gmail.com>
2018-07-29 22:57:09 +0200

    ssss mpt wprlomg

---
+++ Overtone/insane-noises/test/insane_noises/core_test.clj
@@ -0,0 +1,7 @@
+(ns insane-noises.core-test
+  (:require [clojure.test :refer :all]
+            [insane-noises.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

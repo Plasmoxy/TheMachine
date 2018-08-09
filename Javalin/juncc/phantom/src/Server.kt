65d67cf0bc57722f4ef711f8e4008abbcaeeaa53
Plasmoxy <sebohacker@gmail.com>
2018-08-09 22:09:14 +0200

    ok good phantom

--- Javalin/juncc/phantom/src/Server.kt
+++ Javalin/juncc/phantom/src/Server.kt
@@ -1,13 +1,19 @@
 import io.javalin.Javalin
+import io.javalin.embeddedserver.Location
 
 fun main(args: Array<String>) {
+	
+	println("===== PHANTOM ======")
 
 	val app = Javalin.create()
 			.port(args[0].toInt())
+			.enableStaticFiles("static", Location.EXTERNAL)
 			.start()
 
 	app.get("/") {
 		it.html("XDD")
 	}
+	
+	println(" ROUTING DONE ! ")
 
 }

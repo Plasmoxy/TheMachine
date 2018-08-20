ef19871cf657b386377888e06395b8a70586182f
Plasmoxy <sebohacker@gmail.com>
2018-08-20 22:58:11 +0200

    cool files

--- KotlinScript/stuff/showfiles.kts
+++ KotlinScript/stuff/showfiles.kts
@@ -1,3 +1,37 @@
 import java.io.File
 
-File("folder/file1").writeText("XD")
+fun File.rename(name: String) = renameTo(File(absoluteFile.parent, name))
+
+fun list(root: File, level: Int) {
+    
+    // captures level through closure
+    fun padding() = (0 until level).forEach { print("  ") }
+
+    if (!root.isDirectory) {
+        println("This isn't a directory !")
+        return
+    }
+
+    
+
+    val all = root.listFiles()
+    
+    // first directories, recursive
+    all.filter { it.isDirectory }.forEach {
+        padding()
+        println("[${it.name}]")
+        list(it, level + 1)
+    }
+    
+    // then files
+    all.filter { !it.isDirectory }.forEach {
+        padding()
+        println(it.name)
+    }
+}
+
+if (args.size > 0) {
+    list(File(args[0]), 0)
+} else {
+    list(File("folder"), 0)
+}
